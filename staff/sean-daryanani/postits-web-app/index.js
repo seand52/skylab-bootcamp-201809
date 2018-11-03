195499c2539fa02e3ea306a96390230c690b2e6a
Sean Daryanani <seand52@gmail.com>
2018-11-03 18:13:09 +0100

    added pug

--- staff/sean-daryanani/postits-web-app/index.js
+++ staff/sean-daryanani/postits-web-app/index.js
@@ -3,16 +3,16 @@ const express = require('express')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
 const bodyParser = require('body-parser')
-const buildView = require('./helpers/build-view')
 const logic = require('./logic')
+const package = require('./package.json')
 
 const { argv: [, , port = process.env.PORT || 8080] } = process
 
 const app = express()
 
 app.use(express.static('./public'))
+app.set('view engine', 'pug')
 
-let error = null
 
 const formBodyParser = bodyParser.urlencoded({ extended: false })
 
@@ -26,24 +26,18 @@ const mySession = session({
     })
 })
 
+app.use(mySession)
+
 
 app.get('/', (req, res) => {
-    error = null
+    req.session.error = null
 
-    res.send(buildView(`<a href="/login">Login</a> or <a href="/register">Register</a>`))
+    res.render('landing')
 })
 
 app.get('/register', (req, res) => {
 
-    res.send(buildView(`<form action="/register" method="POST">
-            <input type="text" name="name" placeholder="Name">
-            <input type="text" name="surname" placeholder="Surname">
-            <input type="text" name="username" placeholder="username">
-            <input type="password" name="password" placeholder="password">
-            <button type="submit">Register</button>
-        </form>
-        ${error ? `<p class="error">${error}</p>` : ''}
-        <a href="/">go back</a>`))
+    res.render('register', {error: req.session.error})
 })
 
 app.post('/register', formBodyParser, (req, res) => {
@@ -53,18 +47,17 @@ app.post('/register', formBodyParser, (req, res) => {
     try {
         logic.registerUser(name, surname, username, password)
             .then(() => {
-                error = null
+                req.session.error = null
 
-                res.send(buildView(`<p>Ok! user ${name} registered.</p>
-                        <a href="/">go back</a>`))
+            res.render('register-confirm', {name})
             })
             .catch(({message}) => {
-                error = message
+                req.session.error = message
                 res.redirect('/register')
             })       
 
     } catch ({ message }) {
-        error = message
+        req.session.error = message
 
         res.redirect('/register')
     }
@@ -72,14 +65,7 @@ app.post('/register', formBodyParser, (req, res) => {
 
 app.get('/login', (req, res) => {
 
-    res.send(buildView(`<form action="/login" method="POST">
-            <input type="text" name="username" placeholder="username">
-            <input type="password" name="password" placeholder="password">
-            <button type="submit">Login</button>
-        </form>
-
-        ${error ? `<p class="error">${error}</p>` : ''}
-        <a href="/">go back</a>`))
+    res.render('login', {error: req.session.error})
 })
 
 app.post('/login', [formBodyParser, mySession], (req, res) => {
@@ -100,7 +86,7 @@ app.post('/login', [formBodyParser, mySession], (req, res) => {
                 res.redirect('/login')
             })         
     } catch ({ message }) {
-        error = message
+        req.session.error = message
 
         res.redirect('/login')
     }
@@ -114,19 +100,16 @@ app.get('/home', mySession, (req, res) => {
         try {
         logic.retrieveUser(id)
             .then(({name}) => {
-                res.send(buildView(`<p>Welcome ${name}!</p>
-                <a href="/logout">logout</a><br>
-                <a href="/postits">Make Postit</a>
-                `))
+                res.render('home', {name})
 
 
             })
             .catch(({message}) => {
-                error = message
+                req.session.error = message
                 res.redirect('/')
             }) 
         }catch({message}) {
-            error = message
+            req.session.error = message
             res.redirect('/')
         }       
     } else res.redirect('/')
@@ -138,12 +121,6 @@ app.get('/logout', mySession, (req, res) => {
     res.redirect('/')
 })
 
-app.get('/users', (req, res) => {
-    res.send(buildView(`<ul>
-            ${logic._users.map(user => `<li>${user.id} ${user.name} ${user.surname}</li>`).join('')}
-        </ul>
-        <a href="/">go back</a>`))
-})
 
 app.get('/postits', mySession, (req, res) => {
 
@@ -153,27 +130,7 @@ app.get('/postits', mySession, (req, res) => {
         
         logic.retrieveUser(id)
             .then(user => {
-                const listPostits = user.postits.map((item, index) => {
-                    return `<li>
-                    <input type="text" value=${item.postit}>
-                   <form action="/postits/${item.id}" method="POST">
-                    <button type="submit">x</button>
-                    </form>
-                    </li>`
-                }).join('')
-        
-                res.send(buildView(`
-                <p>Welcome ${user.name}!</p>
-                <form action="/postits" method="POST">
-                <input type="text" name="postit"></input>
-                <button type="submit">Create</button>
-                <a href="/logout">logout</a>
-                </form>
-                <ul>
-                    ${listPostits}
-                </ul>
-                
-                `))
+                res.render('postits', {user: user})
             })
             .catch(({message})=>{
                 error = message
@@ -193,7 +150,7 @@ app.post('/postits/:id', mySession, (req, res) => {
             res.redirect('/postits')
         })
         .catch(({message}) => {
-            error = message
+            req.session.error = message
         })
 
 
@@ -211,11 +168,11 @@ app.post('/postits', [formBodyParser, mySession], (req, res) => {
             res.redirect('/postits')
         })
         .catch(({message}) => {
-            error = message
+            req.session.error = message
         })
 
     
 
 })
 
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
+app.listen(port, () => console.log(`Server ${package.version} up and running on port ${port}`))
