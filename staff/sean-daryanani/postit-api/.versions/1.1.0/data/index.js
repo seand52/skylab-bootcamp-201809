b31730a1f7c54a5095e42f757982fd68080afd61
Sean Daryanani <seand52@gmail.com>
2018-11-12 10:27:33 +0100

    removed versions file

--- staff/sean-daryanani/postit-api/.versions/1.1.0/data/index.js
+++
@@ -1,7 +0,0 @@
-const User = require('./user')
-const Postit = require('./postit')
-
-module.exports = {
-    User,
-    Postit
-}
--- staff/sean-daryanani/postit-api/.versions/1.1.0/errors/index.js
+++
@@ -1,9 +0,0 @@
-const AlreadyExistsError = require('./already-exists-error')
-const NotFoundError = require('./not-found-error')
-const ValueError = require('./value-error')
-
-module.exports = {
-    AlreadyExistsError,
-    NotFoundError,
-    ValueError
-}
